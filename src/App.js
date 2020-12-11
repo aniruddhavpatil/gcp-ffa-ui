13503d3a0e0758026a3c62da89263939e55a62fe
aniruddhavpatil <aniruddhavivekpatil@gmail.com>
2020-12-11 14:05:25 -0500

    Added file upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, Input } from '@material-ui/core';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const makeRequest = (file) => {
+  console.log('Reached makeRequest');
+  // console.log('file', file);
+
+  // const url = 'https://hookb.in/Mq6MK39mwBHBKK6OLnVO';
+  // const url = 'http://127.0.0.1:5000';
+
+  const url = 'https://us-central1-gcp-ffa.cloudfunctions.net/gcp_cors';
+  let data = new FormData();
+
+  data.append(file.name, file, file.name);
+  console.log('filename',file.name)
+  axios.post(url, data, {
+    headers: {
+      'accept': 'application/json',
+      'Accept-Language': 'en-US,en;q=0.8',
+      'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
+    }
+  })
+  .then((response) => {
+    console.log(response);
+  }).catch((error) => {
+    //handle error
+  });
+};
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      selectedFile: null
+    };
+  }
+
+  selectFile = event => {
+    // console.log('etf',event.target.files[0])
+    this.setState(
+      {selectedFile: event.target.files[0]}
+    ,
+    () => console.log('selectedfile',this.state.selectedFile)
+    )
+  }
+
+  render(){
+    return(
+    <div>
+      <Input type="file" onChange={(e) => this.selectFile(e)}>Choose File</Input>
+      <Button color="primary" onClick={() => makeRequest(this.state.selectedFile) }>Hello World</Button>
+    </div>);
+  }
+
 }
 
 export default App;
